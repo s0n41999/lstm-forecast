ba9165e000156abad814f08f3c91ecd69c1591f8
s0n41999 <166301423+s0n41999@users.noreply.github.com>
2024-04-07 18:06:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ for i in range(26, len(train_data)):
 x_train, y_train = np.array(x_train), np.array(y_train)
 
 #load model
-model = tf.keras.models.load_model(keras_model.keras)
+model = tf.keras.models.load_model('keras_model.keras')
 
 #making prediction
 test_data = scaled_data[training_data_len - 26: , :] 
