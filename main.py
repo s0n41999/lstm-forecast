60d4b21b63394f1564996eff49920b2dc8da7535
s0n41999 <166301423+s0n41999@users.noreply.github.com>
2024-04-07 22:13:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -87,9 +87,9 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 #model = load_model('keras_model.keras')
 #model=pickle.load(open('model.pkl','rb'))
 def load_model():
-    modell=tf.keras.models.load_model('kera_model.h5')
+    modell=tf.keras.models.load_model('keras_model.keras')
     return modell
-
+    
 model = load_model()
 
 # Train 
